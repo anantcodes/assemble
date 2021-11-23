0a542bf28f1172aafccd1640af8ebb922d9f6e94
Anant Kanchan <anant4265@gmail.com>
2021-11-23 22:40:24 +0530

    :tada: basein commit

---
+++ assemble/assembleApp.swift
@@ -0,0 +1,17 @@
+//
+//  assembleApp.swift
+//  assemble
+//
+//  Created by Anant Kanchan on 23/11/21.
+//
+
+import SwiftUI
+
+@main
+struct assembleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
