8d8a18f398433967aa4b2bab12d39e36b49a0f15
Anant Kanchan <anant4265@gmail.com>
2021-11-25 23:00:47 +0530

    :lipstick: pink tint added

--- assemble/locationButtonView.swift
+++ assemble/locationButtonView.swift
@@ -16,6 +16,7 @@ struct locationButtonView: View {
         ZStack {
             Map(coordinateRegion: $region, showsUserLocation: true)
                 .ignoresSafeArea()
+                .tint(.pink)
         }
     }
 }
