f50f099adec64891b3904225cb3ef89b2938643a
Anant Kanchan <anant4265@gmail.com>
2021-11-25 22:56:48 +0530

    :tada: locationButtonView initiated

---
+++ assemble/locationButtonView.swift
@@ -0,0 +1,26 @@
+//
+//  locationButtonView.swift
+//  assemble
+//
+//  Created by Anant Kanchan on 25/11/21.
+//
+
+import CoreLocationUI
+import MapKit
+import SwiftUI
+
+struct locationButtonView: View {
+    
+    @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 40, longitude: 120), span: MKCoordinateSpan(latitudeDelta: 100, longitudeDelta: 100))
+    var body: some View {
+        ZStack {
+            Map(coordinateRegion: $region, showsUserLocation: true)
+        }
+    }
+}
+
+struct locationButtonView_Previews: PreviewProvider {
+    static var previews: some View {
+        locationButtonView()
+    }
+}
