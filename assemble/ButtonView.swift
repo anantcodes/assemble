2046c870f143b6ac66ff2ab557aef6c84cda88ff
Anant Kanchan <anant4265@gmail.com>
2021-12-06 22:33:31 +0530

    :tada: btn view added

---
+++ assemble/ButtonView.swift
@@ -0,0 +1,25 @@
+//
+//  ButtonView.swift
+//  assemble
+//
+//  Created by Anant Kanchan on 06/12/21.
+//
+
+import SwiftUI
+
+struct ButtonView: View {
+    var body: some View {
+        Button {
+            print("Learn More Tapped")
+        } label: {
+            Text("Learn More")
+        }
+    }
+}
+
+struct ButtonView_Previews: PreviewProvider {
+    static var previews: some View {
+        ButtonView()
+            .preferredColorScheme(.dark)
+    }
+}
