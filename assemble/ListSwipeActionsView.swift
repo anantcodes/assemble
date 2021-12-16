74382dc6787740fbb77d58a3878476a5aeae7222
Anant Kanchan <anant4265@gmail.com>
2021-12-16 20:13:14 +0530

    :tada: listSwipeActionView

---
+++ assemble/ListSwipeActionsView.swift
@@ -0,0 +1,29 @@
+//
+//  ListSwipeActionsView.swift
+//  assemble
+//
+//  Created by Anant Kanchan on 16/12/21.
+//
+
+import SwiftUI
+
+struct ListSwipeActionsView: View {
+    
+    @State var fruits: [String] = [
+        "apple", "orange", "banana", "peach"
+    ]
+    
+    var body: some View {
+        List {
+            ForEach(fruits, id: \.self) {
+                Text($0)
+            }
+        }
+    }
+}
+
+struct ListSwipeActionsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ListSwipeActionsView()
+    }
+}
