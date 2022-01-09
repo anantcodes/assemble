7b97728290509b9712822a7dd0e1724b6f3810bb
Anant Kanchan <anant4265@gmail.com>
2022-01-09 11:08:07 +0530

    :tada: anytransView

---
+++ assemble/AnyTransitionView.swift
@@ -0,0 +1,20 @@
+//
+//  AnyTransitionView.swift
+//  assemble
+//
+//  Created by Anant Kanchan on 09/01/22.
+//
+
+import SwiftUI
+
+struct AnyTransitionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AnyTransitionView_Previews: PreviewProvider {
+    static var previews: some View {
+        AnyTransitionView()
+    }
+}
